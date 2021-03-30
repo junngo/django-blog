d31bd0ed4debb2add41e61fa74544f44f5f4fcf6
junngo <myeongjun.ko@gmail.com>
2021-03-31 00:31:34 +0900

    using CBV instead of FBV

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # /blog/
-    path("", views.index),
-
     # /blog/1/
-    path("<int:pk>/", views.detail),
+    path("<int:pk>/", views.PostDetail.as_view()),
+
+    # /blog/
+    path("", views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
-def index(request):
+class PostList(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    ordering = "-pk"
 
-    posts = Post.objects.all()
 
-    return render(request, "blog/index.html", {"posts": posts},)
-
-def detail(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(request, "blog/post_detail.html", {"post": post})
+class PostDetail(DetailView):
+    model = Post
