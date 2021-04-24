4fe618395075c33a4fe9a7f145830fde2f812136
junngo <myeongjun.ko@gmail.com>
2021-04-24 17:45:36 +0900

    creating category page

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /blog/category/programming/
+    path("category/<str:slug>", views.category_page),
+
     # /blog/1/
     path("<int:pk>/", views.PostDetail.as_view()),
 
