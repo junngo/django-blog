6186bfc35c6f3cba7f95d1cd0433b24c9a39e830
junngo <myeongjun.ko@gmail.com>
2021-05-27 11:09:30 +0900

    set docker environment for deploy

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&-+zn6x$=fe(&lymxt71%+2%d!=s$v$gl52urac2-b)9_)$d)4'
+SECRET_KEY = os.environ.get("SECRET_KEY", '&-+zn6x$=fe(&lymxt71%+2%d!=s$v$gl52urac2-b)9_)$d)4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get("DJANGO_ALLOWED_HOSTS"):
+    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
